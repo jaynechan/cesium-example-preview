b51871c64e50ca749b7971d622ef686cd76ebc66
chenjian <jaynechanbm@gmail.com>
2024-10-07 19:19:22 +0800

    ✨ feat: update

---
+++ static/js/Snow-Enviroment-Effect.7ee6ffbf.js
@@ -0,0 +1,102 @@
+const n=`<template>\r
+  <div class="container">\r
+    <div class="btn_wrapper">\r
+      <el-button type="default" size="small" @click="toggleSnow(true)">开启</el-button>\r
+      <el-button type="primary" size="small" @click="toggleSnow(false)">关闭</el-button>\r
+    </div>\r
+    <div class="cesiumContainer" id="cesiumContainer"></div>\r
+  </div>\r
+</template>\r
+\r
+<script setup>\r
+import * as Cesium from 'cesium'\r
+import { onMounted } from 'vue'\r
+\r
+let snow\r
+onMounted(async () => {\r
+  Cesium.Ion.defaultAccessToken =\r
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmZTA1MDQ2NC0wZmYwLTRhZWMtYWY4OC1jY2JkMDU1NjVmOGMiLCJpZCI6NDM0MzQsImlhdCI6MTY0OTkzNjc0Mn0.nafX1X_3586auU738TC3DxvsiSvPxnQ3TmamqUkb8kw'\r
+  const viewer = new Cesium.Viewer('cesiumContainer', {\r
+    timeline: false, // 是否显示时间轴\r
+    fullscreenButton: false, // 是否显示全屏按钮\r
+    animation: false, // 是否创建动画小器件，左下角仪表\r
+    baseLayerPicker: false, // 是否显示图层选择器\r
+    vrButton: false, // 用于切换 VR 模式的单个按钮小部件。\r
+    geocoder: false, // //是否显示geocoder小器件，右上角查询按钮\r
+    homeButton: false, // 是否显示Home按钮\r
+    infoBox: false, // 是否显示信息框\r
+    sceneModePicker: false, // 是否显示3D/2D选择器\r
+    selectionIndicator: false, // 是否显示选取指示器组件\r
+    navigationHelpButton: false, // 是否显示右上角的帮助按钮\r
+    navigationInstructionsInitiallyVisible: false,\r
+    contextOptions: { webgl: { alpha: true, preserveDrawingBuffer: true }, requestWebgl1: true } // webgl1\r
+  })\r
+  const control = viewer.scene.screenSpaceCameraController\r
+  control.tiltEventTypes = Cesium.CameraEventType.RIGHT_DRAG\r
+  control.zoomEventTypes = [\r
+    Cesium.CameraEventType.WHEEL,\r
+    Cesium.CameraEventType.PINCH\r
+  ]\r
+  // 3dtiles\r
+  const scene = viewer.scene\r
+  const resource = await Cesium.IonResource.fromAssetId(1633341)\r
+  const tileset = await Cesium.Cesium3DTileset.fromUrl(resource)\r
+  changeHeight(tileset, 60)\r
+  scene.primitives.add(tileset)\r
+  viewer.flyTo(tileset, {\r
+    duration: 0\r
+  })\r
+  snow = new Snow(viewer)\r
+  snow.open()\r
+})\r
+\r
+function toggleSnow(flag) {\r
+  if (flag) {\r
+    snow.open()\r
+  } else {\r
+    snow.close()\r
+  }\r
+}\r
+\r
+function changeHeight(tileset, height) {\r
+  const cartographic = Cesium.Cartographic.fromCartesian(\r
+    tileset.boundingSphere.center\r
+  )\r
+  const surface = Cesium.Cartesian3.fromRadians(\r
+    cartographic.longitude,\r
+    cartographic.latitude,\r
+    0.0\r
+  )\r
+  const offset = Cesium.Cartesian3.fromRadians(\r
+    cartographic.longitude,\r
+    cartographic.latitude,\r
+    height\r
+  )\r
+  const translation = Cesium.Cartesian3.subtract(\r
+    offset,\r
+    surface,\r
+    new Cesium.Cartesian3()\r
+  )\r
+  tileset.modelMatrix = Cesium.Matrix4.fromTranslation(translation)\r
+}\r
+<\/script>\r
+<style scoped>\r
+.container {\r
+  position: relative;\r
+  width: 100%;\r
+  height: 100%;\r
+  .cesiumContainer {\r
+    width: 100%;\r
+    height: 100%;\r
+    overflow: hidden\r
+  }\r
+  .btn_wrapper {\r
+    position: absolute;\r
+    left: 50px;\r
+    top: 10px;\r
+    z-index: 1;\r
+  }\r
+}\r
+\r
+</style>\r
+`;export{n as default};
---
+++ static/js/index.BxAkc5VZ.js
@@ -0,0 +1,247 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./index.26f61cf4.js","./monaco-editor.D3ig2Cuf.js","../css/monaco-editor.BzWl1eyH.css","./vue.ClgfzGxZ.js","./@vue.Lfky3Lsd.js","./@vueuse.BGoRStkq.js","./vue3-sfc-loader.CoUr1SK8.js","./@turf.BhhZGEnx.js","./point-in-polygon-hao.CVtqB2KT.js","./sweepline-intersections.D78iGcFZ.js","./geojson-equality-ts.385IDfwq.js","./fast-deep-equal.BZWWD7KU.js","./d3-geo.B8ycIn1u.js","./d3-array.0UpHvbR7.js","./concaveman.D4rCrqp7.js","./tinyqueue.BpsZqhqS.js","./point-in-polygon.BrHNa-um.js","./robust-predicates.DbhKlCUI.js","./skmeans.Df_G8q9F.js","./topojson-client.C_Q_XF1Z.js","./topojson-server.BJ5ICLc2.js","./polygon-clipping.C7RG_Bzj.js","./splaytree.t-pFliWO.js","./marchingsquares.DR8wX6S2.js","./d3-voronoi.DD4zjRyK.js","./dat.gui.kzZ7KUwl.js","./vue-router.bYO7SHUI.js","./element-plus.BHdALmn5.js","./lodash-es.CiJSjksT.js","./@element-plus.DY0ir03P.js","./@popperjs.D9SI2xQl.js","./@ctrl.r5W6hzzQ.js","./dayjs.Dldw1-ZD.js","./async-validator.DKvM95Vc.js","./memoize-one.BdPwpGay.js","./normalize-wheel-es.B6fDCfyv.js","./@floating-ui.8uccrNCM.js","../css/element-plus.B2tId5af.css","./pinia.BCCKnU4o.js","../css/index.BrWGY-An.css","../css/cesium.BHwS5cQ6.css","./index.956e4a2a.js","../css/index.CM5F9i6p.css"])))=>i.map(i=>d[i]);
+var g=Object.defineProperty;var _=(t,e,o)=>e in t?g(t,e,{enumerable:!0,configurable:!0,writable:!0,value:o}):t[e]=o;var i=(t,e,o)=>_(t,typeof e!="symbol"?e+"":e,o);import"./vue.ClgfzGxZ.js";/* empty css               */import{E as w,i as y,z as x}from"./element-plus.BHdALmn5.js";import{d as h,M as C,u as S,ai as P,o as f,O as b,U as k,c as m,a as D,I as z,au as B}from"./@vue.Lfky3Lsd.js";import{c as F}from"./pinia.BCCKnU4o.js";import{_ as d}from"./monaco-editor.D3ig2Cuf.js";import{c as L,a as A}from"./vue-router.bYO7SHUI.js";import{Q as T}from"./@element-plus.DY0ir03P.js";import{d as E}from"./dat.gui.kzZ7KUwl.js";import"./lodash-es.CiJSjksT.js";import"./@vueuse.BGoRStkq.js";import"./@popperjs.D9SI2xQl.js";import"./@ctrl.r5W6hzzQ.js";import"./dayjs.Dldw1-ZD.js";import"./@turf.BhhZGEnx.js";import"./point-in-polygon-hao.CVtqB2KT.js";import"./sweepline-intersections.D78iGcFZ.js";import"./geojson-equality-ts.385IDfwq.js";import"./fast-deep-equal.BZWWD7KU.js";import"./d3-geo.B8ycIn1u.js";import"./d3-array.0UpHvbR7.js";import"./concaveman.D4rCrqp7.js";import"./tinyqueue.BpsZqhqS.js";import"./point-in-polygon.BrHNa-um.js";import"./robust-predicates.DbhKlCUI.js";import"./skmeans.Df_G8q9F.js";import"./topojson-client.C_Q_XF1Z.js";import"./topojson-server.BJ5ICLc2.js";import"./polygon-clipping.C7RG_Bzj.js";import"./splaytree.t-pFliWO.js";import"./marchingsquares.DR8wX6S2.js";import"./d3-voronoi.DD4zjRyK.js";import"./async-validator.DKvM95Vc.js";import"./memoize-one.BdPwpGay.js";import"./normalize-wheel-es.B6fDCfyv.js";import"./@floating-ui.8uccrNCM.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))a(r);new MutationObserver(r=>{for(const s of r)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(r){const s={};return r.integrity&&(s.integrity=r.integrity),r.referrerPolicy&&(s.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?s.credentials="include":r.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(r){if(r.ep)return;r.ep=!0;const s=o(r);fetch(r.href,s)}})();const R=h({__name:"App",setup(t){const e="production",a="https://cesium.com/downloads/cesiumjs/releases/1.120/Build/Cesium/";return window.CESIUM_BASE_URL=a,console.log(`模式: ${e}, CESIUM_BASE_URL: ${a}`),(r,s)=>{const n=P("router-view");return f(),C(S(w),{size:"default"},{default:b(()=>[k(n)]),_:1})}}}),v=(t,e)=>{const o=t.__vccOpts||t;for(const[a,r]of e)o[a]=r;return o},V=v(R,[["__scopeId","data-v-6ec92aea"]]),I=F();function U(t){t.use(I)}const O=()=>d(()=>import("./index.26f61cf4.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]),import.meta.url),M=()=>d(()=>import("./index.956e4a2a.js"),__vite__mapDeps([41,3,4,38,27,28,5,29,30,31,32,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,33,34,35,36,37,1,2,26,25,42,40]),import.meta.url),N=[{path:"/",redirect:"/example"},{path:"/example",component:M,name:"example"},{path:"/example/preview",component:O,name:"preview"}],$=L({history:A(),routes:N,scrollBehavior:()=>({left:0,top:0})}),j=h({name:"IconSvg",props:{value:{type:String,required:!0}}}),G={key:0,class:"com-icon"},H={class:"com-icon-svg","aria-hidden":"true"},W=["xlink:href"];function q(t,e,o,a,r,s){return t.value.indexOf("#")===0?(f(),m("i",G,[(f(),m("svg",H,[D("use",{"xlink:href":t.value},null,8,W)]))])):(f(),m("i",{key:1,class:z(t.value)},null,2))}const K=v(j,[["render",q],["__scopeId","data-v-f8504d79"]]),l=window.Cesium,Q=`
+  uniform sampler2D colorTexture;
+  uniform bool showLightning;
+  in vec2 v_textureCoordinates;
+  
+
+  float rand(float x)
+  {
+    return fract(sin(x)*75154.32912);
+  }
+
+  // 使用Perlin噪声生成3D伪随机噪声
+  float rand3d(vec3 x)
+  {
+    return fract(375.10297*sin(dot(x,vec3(103.0139,227.0595,31.05914))));
+  }
+  // 生成1D Perlin噪声
+  float noise(float x)
+  {
+    float i=floor(x);
+    float a=rand(i),b=rand(i+1.);
+    float f=x-i;
+    return mix(a,b,f);
+  }
+
+  // 结合多个1D Perlin噪声的八度
+  float perlin(float x)
+  {
+    float r=0.,s=1.,w=1.;
+    for(int i=0;i<6;i++){
+      s*=2.;
+      w*=.5;
+      r+=w*noise(s*x);
+    }
+    return r;
+  }
+  // 生成3D Perlin噪声
+  float noise3d(vec3 x)
+  {
+    vec3 i=floor(x);
+    float i000=rand3d(i+vec3(0.,0.,0.)),i001=rand3d(i+vec3(0.,0.,1.));
+    float i010=rand3d(i+vec3(0.,1.,0.)),i011=rand3d(i+vec3(0.,1.,1.));
+    float i100=rand3d(i+vec3(1.,0.,0.)),i101=rand3d(i+vec3(1.,0.,1.));
+    float i110=rand3d(i+vec3(1.,1.,0.)),i111=rand3d(i+vec3(1.,1.,1.));
+    vec3 f=x-i;
+    return mix(mix(mix(i000,i001,f.z),mix(i010,i011,f.z),f.y),
+    mix(mix(i100,i101,f.z),mix(i110,i111,f.z),f.y),f.x);
+  }
+
+  // 结合多个3D Perlin噪声的八度
+  float perlin3d(vec3 x)
+  {
+    float r=0.;
+    float w=1.,s=1.;
+    for(int i=0;i<5;i++){
+      w*=.5;
+      s*=2.;
+      r+=w*noise3d(s*x);
+    }
+    return r;
+  }
+
+  // 基于Perlin噪声生成闪电形状
+  float f(float y)
+  {
+    float w=.4;// 走向宽度
+    return w*(perlin(2.*y)-.5);
+  }
+
+  // 绘制闪电，可选择增加厚度
+  float plot(vec2 p,float d,bool thicker)
+  {
+    if(thicker)d+=5.*abs(f(p.y+.001)-f(p.y));
+    return smoothstep(d,0.,abs(f(p.y)-p.x));
+  }
+
+  // 基于3D Perlin噪声生成云效果
+  float cloud(vec2 uv,float speed,float scale,float cover)
+  {
+    float iTime=czm_frameNumber*.008;
+    float c=perlin3d(vec3(uv*scale,iTime*speed*2.));
+    return max(0.,c-(1.-cover));
+  }
+
+  vec3 render(vec2 uv)
+  {
+    float iTime=czm_frameNumber*.008;
+    float x=iTime+.1;
+    float m=.25;// 闪电形状持续时间
+    float i=floor(x/m);
+    float f=x/m-i;
+    float k=.4;// 闪电间隔/频率
+    float n=noise(i);
+    float t=ceil(n-k);
+    float d=max(0.,n-k)/(1.-k);
+    float o=ceil(t-f-(1.-d));
+    float gt=.1;
+    float go=ceil(t-f-(1.-gt));
+
+    float lightning=0.;
+    float light=0.;
+    float glare=0.;
+    if(o==1.){
+      vec2 uv2=uv;
+      uv2.y+=i*2.;
+      float p=(noise(i+10.)-.5)*2.;
+      uv2.x-=p;
+
+      float strike=plot(uv2,.01,true);
+      float glow=plot(uv2,.04,false);
+      float glow2=plot(uv2,1.5,false);
+
+      lightning=strike*.4+glow*.15;
+
+      float h=noise(i+5.);
+      lightning*=smoothstep(h,h+.05,uv.y+perlin(1.2*uv.x+4.*h)*.03);
+      lightning+=glow2*.3;
+      light=smoothstep(5.,0.,abs(uv.x-p));
+      glare=go*light;
+    }
+
+    vec3 clouds=
+    vec3(.5,.7,1.)*mix(.6,.9,cloud(uv,.2,.1,1.))+
+    vec3(.7,.8,1.)*.6*cloud(uv*vec2(.5,1.),.06,.8,.8)+
+    vec3(.9,.9,1.)*.3*cloud(uv*vec2(.1,1.),.08,5.5,.6)+
+    vec3(1.,1.,1.)*.4*cloud(uv*vec2(.1,1.),.07,10.,.5);
+
+    vec3 background=vec3(.8);//背景颜色
+    background*=(.2+light*.5);
+    if(showLightning == true){
+      return vec3(background+lightning+glare);
+    }else {
+      return vec3(background+glare);
+    }
+    
+  }
+
+  void main()
+  {
+    vec2 iResolution=czm_viewport.zw;
+    vec2 uv=(gl_FragCoord.xy*2.-iResolution.xy)/iResolution.y;
+
+    out_FragColor=vec4(render(uv),1.);
+
+    vec4 sceneColor=texture(colorTexture,v_textureCoordinates);
+    out_FragColor=mix(out_FragColor,sceneColor,.5);
+  }
+`;class J{constructor(e){i(this,"_viewer");i(this,"showLightning",!1);i(this,"_lighting");this._viewer=e}init(){this._lighting=new l.PostProcessStage({name:"weather_lighting",fragmentShader:Q,uniforms:{fogByDistance:new l.Cartesian4(10,0,1e3,.8),fogColor:l.Color.WHITE,showLightning:()=>this.showLightning}}),this._viewer.scene.postProcessStages.add(this._lighting),this._viewer.scene.postRender.addEventListener(()=>{const e=l.Math.toDegrees(this._viewer.camera.pitch);this.showLightning=e>-15})}destroy(){var e;this.clear(),(e=this._lighting)==null||e.destroy()}clear(){!this._viewer||!this._lighting||this._viewer.scene.postProcessStages.remove(this._lighting)}show(e){e?this.init():this.clear()}}const u=window.Cesium;class X{constructor(e,o){i(this,"viewer");i(this,"angle");i(this,"size");i(this,"speed");i(this,"rainStage");if(!e)throw new Error("no viewer object!");o=o||{},this.angle=u.defaultValue(o.angle,-.6),this.size=u.defaultValue(o.size,.1),this.speed=u.defaultValue(o.speed,1e3),this.viewer=e,this.init()}show(e){this.rainStage.enabled=e}init(){this.rainStage=new u.PostProcessStage({name:"czml_rain",fragmentShader:this.rain(),uniforms:{angle:()=>this.angle,size:()=>this.size,speed:()=>this.speed}}),this.viewer.scene.postProcessStages.add(this.rainStage)}destroy(){!this.viewer||!this.rainStage||this.viewer.scene.postProcessStages.remove(this.rainStage)}rain(){return`uniform sampler2D colorTexture;
+              varying vec2 v_textureCoordinates;
+              uniform float angle;
+              uniform float size;
+              uniform float speed;
+              float hash(float x){
+                  return fract(sin(x*133.3)*13.13);
+          }
+          void main(void){
+              float time = czm_frameNumber / speed;
+              vec2 resolution = czm_viewport.zw;
+              vec2 uv=(gl_FragCoord.xy*2.-resolution.xy)/min(resolution.x,resolution.y);
+              vec3 c=vec3(.6,.7,.8);
+              float a=angle;
+              float si=sin(a),co=cos(a);
+              uv*=mat2(co,-si,si,co);
+              uv*=length(uv+vec2(0,4.9))*size + 1.;
+              float v=1.-sin(hash(floor(uv.x*100.))*2.);
+              float b=clamp(abs(sin(20.*time*v+uv.y*(5./(2.+v))))-.95,0.,1.)*20.;
+              c*=v*b; 
+              gl_FragColor = mix(texture2D(colorTexture, v_textureCoordinates), vec4(c,1), 0.5);  
+          }
+           `}}const Y=window.Cesium.Cartesian4,Z=window.Cesium.Color,ee=window.Cesium.PostProcessStage;class te{constructor(e){i(this,"viewer");i(this,"collection");i(this,"postProcessState");i(this,"_fogByDistance",{near:100,nearValue:.35,far:4e3,farValue:.9});i(this,"_color",new Z(1,1,1,1));i(this,"_visibility",.4);this.viewer=e,this.collection=e.scene.postProcessStages}set fogByDistance(e){this._fogByDistance=e}get fogByDistance(){return this._fogByDistance}set color(e){this._color=e}get color(){return this._color}get visibility(){return this._visibility}set visibility(e){this._visibility=e}_createPostProcessStage(){var e,o,a,r;this.postProcessState=new ee({name:"czm_fog",fragmentShader:this._getFs(),uniforms:{fogColor:()=>this._color,visibility:()=>this._visibility,fogByDistance:new Y(((e=this._fogByDistance)==null?void 0:e.near)||10,((o=this._fogByDistance)==null?void 0:o.nearValue)||0,((a=this._fogByDistance)==null?void 0:a.far)||200,((r=this._fogByDistance)==null?void 0:r.farValue)||1)}}),this.collection.add(this.postProcessState)}_getFs(){return`float getDistance(sampler2D depthTexture, vec2 texCoords)
+              {
+                  float depth = czm_unpackDepth(texture(depthTexture, texCoords));
+                  if (depth == 0.0) {
+                      return czm_infinity;
+                  }
+                  vec4 eyeCoordinate = czm_windowToEyeCoordinates(gl_FragCoord.xy, depth);
+                  return -eyeCoordinate.z / eyeCoordinate.w;
+              }
+              //根据距离，在中间进行插值
+              float interpolateByDistance(vec4 nearFarScalar, float distance)
+              {
+                  //根据常识，雾应该是距离远，越看不清，近距离内的物体可以看清
+                  //因此近距离alpha=0，远距离的alpha=1.0
+                  //本例中设置可见度为200米
+                  //雾特效的起始距离
+                  float startDistance = nearFarScalar.x;
+                  //雾特效的起始alpha值
+                  float startValue = nearFarScalar.y;
+                  //雾特效的结束距离
+                  float endDistance = nearFarScalar.z;
+                  //雾特效的结束alpha值
+                  float endValue = nearFarScalar.w;
+                  //根据每段距离占总长度的占比，插值alpha，距离越远，alpha值越大。插值范围0,1。
+                  float t = clamp((distance - startDistance) / (endDistance - startDistance), 0.0, 1.0);
+                  return mix(startValue, endValue, t);
+              }
+              vec4 alphaBlend(vec4 sourceColor, vec4 destinationColor)
+              {
+                  return sourceColor * vec4(sourceColor.aaa, 1.0) + destinationColor * (1.0 - sourceColor.a);
+              }
+              uniform sampler2D colorTexture;
+              uniform sampler2D depthTexture;
+              uniform vec4 fogByDistance;
+              uniform vec4 fogColor;
+              in vec2 v_textureCoordinates;
+              void main(void)
+              {
+                  //获取地物距相机的距离
+                  float distance = getDistance(depthTexture, v_textureCoordinates);
+                  //获取场景原本的纹理颜色
+                  vec4 sceneColor = texture(colorTexture, v_textureCoordinates);
+                  //根据距离，对alpha进行插值
+                  float blendAmount = interpolateByDistance(fogByDistance, distance);
+                  //将alpha变化值代入雾的原始颜色中，并将雾与场景原始纹理进行融合
+                  vec4 finalFogColor = vec4(fogColor.rgb, fogColor.a * blendAmount);
+                  out_FragColor = alphaBlend(finalFogColor, sceneColor);
+              }`}open(){const e=this.viewer.scene;this.postProcessState||(this._createPostProcessStage(),e.skyAtmosphere.hueShift=0,e.skyAtmosphere.saturationShift=0,e.skyAtmosphere.brightnessShift=.7,e.fog.density=5e-4,e.fog.minimumBrightness=.03)}close(){const e=this.viewer.scene;this.postProcessState&&(this.collection.remove(this.postProcessState),this.postProcessState=void 0,e.skyAtmosphere.hueShift=0,e.skyAtmosphere.saturationShift=0,e.skyAtmosphere.brightnessShift=.4,e.fog.density=2e-4,e.fog.minimumBrightness=.03)}}const oe=window.Cesium.PostProcessStage;class re{constructor(e){i(this,"viewer");i(this,"collection");i(this,"speed",80);i(this,"size",.02);i(this,"postProcessState");this.viewer=e,this.collection=e.scene.postProcessStages}_createPostProcessStage(){this.postProcessState=new oe({name:"czm_snow",fragmentShader:this._getFs(),uniforms:{speed:()=>this.speed,size:()=>this.size}}),this.collection.add(this.postProcessState)}_getFs(){return`uniform sampler2D colorTexture;
+    varying vec2 v_textureCoordinates;
+    uniform float speed;
+    uniform float size;
+    float snow(vec2 uv,float scale)
+    {
+        float time = czm_frameNumber / speed;
+        float w=smoothstep(1.,0.,-uv.y*(scale/10.));if(w<.1)return 0.;
+        uv+=time/scale;uv.y+=time*2./scale;uv.x+=sin(uv.y+time*.5)/scale;
+        uv*=scale;vec2 s=floor(uv),f=fract(uv),p;float k=3.,d;
+        p=.5+.35*sin(11.*fract(sin((s+p+scale)*mat2(7,3,6,5))*5.))-f;d=length(p);k=min(d,k);
+        k=smoothstep(0.,k,sin(f.x+f.y)*size);
+        return k*w;
+    }
+    void main(void){
+        vec2 resolution = czm_viewport.zw;
+        vec2 uv=(gl_FragCoord.xy*2.-resolution.xy)/min(resolution.x,resolution.y);
+        vec3 finalColor=vec3(0);
+        float c = 0.0;
+        c+=snow(uv,30.)*.0;
+        c+=snow(uv,20.)*.0;
+        c+=snow(uv,15.)*.0;
+        c+=snow(uv,10.);
+        c+=snow(uv,8.);
+        c+=snow(uv,6.);
+        c+=snow(uv,5.);
+        finalColor=(vec3(c)); 
+        gl_FragColor = mix(texture2D(colorTexture, v_textureCoordinates), vec4(finalColor,1), 0.5); 
+    }
+`}open(){const e=this.viewer.scene;this.postProcessState||(this._createPostProcessStage(),e.skyAtmosphere.hueShift=-.8,e.skyAtmosphere.saturationShift=-.7,e.skyAtmosphere.brightnessShift=-.33,e.fog.density=.001,e.fog.minimumBrightness=.8)}close(){const e=this.viewer.scene;this.postProcessState&&(this.collection.remove(this.postProcessState),this.postProcessState=void 0,e.skyAtmosphere.hueShift=0,e.skyAtmosphere.saturationShift=0,e.skyAtmosphere.brightnessShift=.4,e.fog.density=2e-4,e.fog.minimumBrightness=.03)}}const ie=new URL(""+new URL("../img/world_b.CKToTiVD.jpg",import.meta.url).href,import.meta.url).href,se=new URL(""+new URL("../img/world_img.BGGjUUzF.jpg",import.meta.url).href,import.meta.url).href,ae=new URL(""+new URL("../img/backGroundImg.CHcdoOpt.jpg",import.meta.url).href,import.meta.url).href,ne={dat:E,Lightning:J,Rain:X,Fog:te,Snow:re,BaseMapWorld_Blue:ie,BaseMapWorld_Image:se,SceneBackGroundImg:ae};for(const[t,e]of Object.entries(ne))Reflect.defineProperty(window,t,{value:e});const p=document.createElement("link");p.rel="stylesheet";p.href="//at.alicdn.com/t/c/font_4702543_kgiwod8u1v.css";document.head.appendChild(p);const c=B(V);U(c);for(const[t,e]of Object.entries(T))c.component(t,e);c.component("IconSvg",K);c.use(y,{locale:x});c.use($).mount("#app");export{v as _};
